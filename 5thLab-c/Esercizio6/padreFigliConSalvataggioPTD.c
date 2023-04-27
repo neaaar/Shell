e9781ae6412f12f95d879227dc80783610cac804
neaaar <marco.farioli@outlook.it>
2023-04-27 17:13:27 +0200

    Update

--- 5thLab-c/Esercizio6/padreFigliConSalvataggioPTD.c
+++ 5thLab-c/Esercizio6/padreFigliConSalvataggioPTD.c
@@ -31,10 +31,39 @@ int main(int argc, char* argv[]) {
             printf("Stampa del processo figlio numero %d, il cui pid è %d\n", i, getpid());
 
             srand(time(NULL));
-            int rnd = rand() % (100 + i);
+            int rnd = rand() % (101 + i);
             exit(rnd);
         }
     }
+
+    int *pid;
+    if ((pid=(int *)malloc(N*sizeof(int))) == NULL)	/* bisogna sempre controllare che la malloc abbia avuto successo! */
+	{
+        	printf("Errore allocazione pid\n");
+        	exit(3);
+	}
+
+    for (i=0; i < N; i++)
+	{
+		if ((pidFiglio=wait(&status)) < 0)
+        	{
+                	printf("Errore in wait %d\n", pidFiglio);
+                	exit(5);
+        	}
+        	if ((status & 0xFF) != 0)
+                	printf("Figlio con pid %d terminato in modo anomalo\n", pidFiglio);
+        	else
+        	{ 
+			ritorno=(int)((status >> 8) & 0xFF);
+			/* la specifica richiede che il padre, oltre il valore di ritorno e il PID, stampi anche il numero d'ordine e questo puo' essere fatto solo analizzando il contenuto dell'array di pid e riportando l'indice di tale array. ATTENZIONE CHE SAREBBE ASSOLUTAMENTE ERRATO ANDARE A STAMPARE L'INDICE DEL FOR ESTERNO! */
+			for (j=0; j < N; j++)
+				if (pidFiglio == pid[j])
+				{
+        				printf("Il figlio con pid=%d di indice %d ha ritornato %d\n", pidFiglio, j, ritorno);	/* attenzione a stampare j e NON i!!! */
+					break;
+				}	
+        	}	 
+	}
     
     exit(0);
 }
